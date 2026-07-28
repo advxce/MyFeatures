1aa9beb1a010c195aadec95d306231342ecd0bf1
Dima <poznyak.dima2018@gmail.com>
2026-07-28 16:19:51 +0300

    add exo player

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -56,4 +57,7 @@ dependencies {
     implementation(project(":download_manager_feature"))
     implementation(project(":exo_player_feature"))
     implementation(project(":core:navigation"))
+
+    implementation(platform("com.google.firebase:firebase-bom:34.16.0"))
+
 }
